d7463ef37b0a682e82f1b25473bdec597de9f871
Shania <zixuan_guo@brown.edu--global user.name zguo47>
2023-04-14 03:22:08 -0400

    new

--- code/models.py
+++ code/models.py
@@ -57,23 +57,62 @@ class YourModel(tf.keras.Model):
 
         self.architecture = [
               ## Add layers here separated by commas.
-              Conv2D(32, (3, 3), 1, padding="same",
-                   activation='leaky_relu'),
-              Conv2D(32, (3, 3), 1, padding="same",
-                   activation='leaky_relu'),
-              MaxPool2D((2, 2), 2),
-              Conv2D(64, (3, 3), 1, padding="same", activation='leaky_relu'),
-              Conv2D(64, (3, 3), 1, padding="same", activation='leaky_relu'),
-              MaxPool2D((2, 2), 2),
-              Conv2D(128, (5, 5), 1, padding="same", activation='leaky_relu'),
-              Conv2D(128, (5, 5), 1, padding="same", activation='leaky_relu'),
-              MaxPool2D((2, 2), 2),
-              Flatten(),
-              Dense(64, activation='sigmoid'),
-              Dropout(0.1, dtype = 'float32'), 
-              Dense(32, activation='sigmoid'),
-              Dropout(0.1, dtype = 'float32'), 
-              Dense(15, activation='softmax')
+              # Conv2D(32, (3, 3), 1, padding="same",
+              #      activation='leaky_relu'),
+              # Conv2D(32, (3, 3), 1, padding="same",
+              #      activation='leaky_relu'),
+              # MaxPool2D((2, 2), 2),
+              # Conv2D(64, (3, 3), 1, padding="same", activation='leaky_relu'),
+              # Conv2D(64, (3, 3), 1, padding="same", activation='leaky_relu'),
+              # MaxPool2D((2, 2), 2),
+              # Conv2D(128, (5, 5), 1, padding="same", activation='leaky_relu'),
+              # Conv2D(128, (5, 5), 1, padding="same", activation='leaky_relu'),
+              # MaxPool2D((2, 2), 2),
+              # Flatten(),
+              # Dense(64, activation='sigmoid'),
+              # Dropout(0.1, dtype = 'float32'), 
+              # Dense(32, activation='sigmoid'),
+              # Dropout(0.1, dtype = 'float32'), 
+              # Dense(15, activation='softmax')
+
+              
+            # Block 1
+            Conv2D(64, 3, 1, padding="same",
+                   activation="relu", name="block1_conv1"),
+            Conv2D(64, 3, 1, padding="same",
+                   activation="relu", name="block1_conv2"),
+            MaxPool2D(2, name="block1_pool"),
+            # Block 2
+            Conv2D(128, 3, 1, padding="same",
+                   activation="relu", name="block2_conv1"),
+            Conv2D(128, 3, 1, padding="same",
+                   activation="relu", name="block2_conv2"),
+            MaxPool2D(2, name="block2_pool"),
+            # Block 3
+            Conv2D(256, 3, 1, padding="same",
+                   activation="relu", name="block3_conv1"),
+            Conv2D(256, 3, 1, padding="same",
+                   activation="relu", name="block3_conv2"),
+            Conv2D(256, 3, 1, padding="same",
+                   activation="relu", name="block3_conv3"),
+            MaxPool2D(2, name="block3_pool"),
+            # Block 4
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block4_conv1"),
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block4_conv2"),
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block4_conv3"),
+            MaxPool2D(2, name="block4_pool"),
+            # Block 5
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block5_conv1"),
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block5_conv2"),
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block5_conv3"),
+            MaxPool2D(2, name="block5_pool")
+        
         ]
 
     def call(self, x):
