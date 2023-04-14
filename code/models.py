1404021aab49e637d527aa557e3c85319eff7f1b
Shania <zixuan_guo@brown.edu--global user.name zguo47>
2023-04-14 00:50:12 -0400

    new

--- code/models.py
+++ code/models.py
@@ -20,7 +20,7 @@ class YourModel(tf.keras.Model):
         # TASK 1
         # TODO: Select an optimizer for your network (see the documentation
         #       for tf.keras.optimizers)
-        self.optimizer = tf.keras.optimizers.Adam(0.001)
+        self.optimizer = tf.keras.optimizers.Adam(0.1)
 
         # TASK 1
         # TODO: Build your own convolutional neural network with a 
@@ -62,10 +62,10 @@ class YourModel(tf.keras.Model):
               Conv2D(64, (4, 4), activation='leaky_relu',  strides=(2, 2), dtype = 'float32'),
               Conv2D(128, (7,7), activation='leaky_relu', dtype = 'float32'),
               Flatten(),
-              Dense(64, activation='leaky_relu', dtype = 'float32'),
-              Dropout(0.1, dtype = 'float32'), 
-              Dense(32, activation='sigmoid', dtype = 'float32'),
-              Dropout(0.1, dtype = 'float32'), 
+              # Dense(64, activation='leaky_relu', dtype = 'float32'),
+              # Dropout(0.1, dtype = 'float32'), 
+              Dense(32, activation='leaky_relu', dtype = 'float32'),
+              # Dropout(0.1, dtype = 'float32'), 
               Dense(15, activation='softmax', dtype = 'float32')
         ]
 
